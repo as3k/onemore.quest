0292d88b03062a64145aca7a8142b7b00ad66f87
as3k <zack.grrr@gmail.com>
2022-11-22 00:35:59 -0800

    initial commit

---
+++ pages/_document.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="bg-neutral-800 text-neutral-200 min-height-screen">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
